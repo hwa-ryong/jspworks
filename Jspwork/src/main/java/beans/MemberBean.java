808368820a1221c64c754536a1095a5c593eb587
hwa-ryong <ocsohy@naver.com>
2023-05-30 16:48:13 +0900

    jsp

---
+++ Jspwork/src/main/java/beans/MemberBean.java
@@ -0,0 +1,35 @@
+package beans;
+
+import java.io.Serializable;
+
+//인터페이스 Serializable(직렬화 - 현재 객체 자체를 저장)
+public class MemberBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private int id = 2023101;
+	private String name = "장그래";
+	
+	//생성자
+	public MemberBean() {}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
