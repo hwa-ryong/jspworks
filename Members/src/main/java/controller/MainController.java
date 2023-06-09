544f508c0a2c70c29c637e3e411c87a4a8ed2ff2
hwa-ryong <ocsohy@naver.com>
2023-06-09 17:40:19 +0900

    form

---
+++ Members/src/main/java/controller/MainController.java
@@ -0,0 +1,83 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.Member;
+import member.MemberDAO;
+
+
+@WebServlet("*.do")  //경로를 .do로 끝나도록 설정
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 4L;
+
+	MemberDAO memberDAO;    //MemberDAO 객체 선언
+	
+	public void init(ServletConfig config) throws ServletException {
+		memberDAO = new MemberDAO();  //객체 생성
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("utf-8");
+		
+		//command 패턴으로 url 설정하기
+		String uri = request.getRequestURI();
+		System.out.println(uri);
+		String command = uri.substring(uri.lastIndexOf('/'));
+		System.out.println(uri.lastIndexOf('/'));
+		System.out.println("command: " + command);
+		
+		String nextPage = null;
+		
+		//회원 목록 조회
+		if(command.equals("/memberList.do")) {
+			
+			ArrayList<Member> memberList = memberDAO.getMemberList();
+			
+			//모델 생성 및 보내기 
+			request.setAttribute("memberList", memberList);
+			nextPage = "memberList.jsp";
+		}else if(command.equals("/memberForm.do")) {
+			nextPage = "memberForm.jsp";
+		}else if(command.equals("/addMember.do")) {
+			//폼에 입력된 데이터 받기
+			String memberId = request.getParameter("memberId");
+			String passwd = request.getParameter("passwd1");
+			String name = request.getParameter("name");
+			String gender = request.getParameter("gender");
+			
+			Member newMember = new Member();  //회원 객체 생성
+			newMember.setMemberId(memberId);
+			newMember.setPasswd(passwd);
+			newMember.setName(name);
+			newMember.setGender(gender);
+			
+			
+			memberDAO.addMember(newMember);   //회원 매개로 DB에 저장
+			nextPage = "index.jsp";
+		}
+		
+		//포워딩
+		RequestDispatcher dispatcher =
+				request.getRequestDispatcher(nextPage);
+		
+		dispatcher.forward(request, response);
+		
+	}
+
+}
---
+++ Members/src/main/webapp/memberForm.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>회원 가입</title>
+<link rel="stylesheet" href="resources/css/style.css">
+</head>
+	<body>
+		<div id="container">
+			<section id="register">
+				<h2>회원 가입</h2>
+				<form action="addMember.do" method="post">
+					<fieldset>
+						<ul>
+							<li>
+								<label for="memberId">아이디</label> <input type="text"
+									id="memberId" name="memberId">
+							</li>
+							<li>
+								<label for="passwd1">비밀번호</label> 
+								<input type="password" id="passwd1" name="passwd1">
+							</li>
+							<li>
+								<label for="passwd2">비밀번호 확인</label> <input type="password"
+									id="passwd2" name="passwd2">
+							</li>
+							<li>
+								<label for="name">이름</label> <input type="text" id="name"
+									name="name">
+							</li>
+							<li>
+								<label for="name">성별</label> <label class="radio"> <input
+									type="radio" name="gender" value="남" checked>남
+								</label> <label class="radio"> <input type="radio" name="gender"
+									value="여" checked>여
+								</label>
+							</li>
+							<li>
+								<input type="submit" value="가입하기"> <input type="reset"
+									value="다시쓰기">
+							</li>
+						</ul>
+					</fieldset>
+				</form>
+			</section>
+		</div>
+	</body>
+</html>
